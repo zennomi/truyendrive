f37cdb91f5573d75fba87faeb2acaf68ebe641e3
Zennomi <43679352+zennomi@users.noreply.github.com>
2026-05-12 22:48:47 +0700

    refactor: replace MultiStateButton with SVG icon in ReaderSidebar password button

--- src/components/ReaderSidebar.tsx
+++ src/components/ReaderSidebar.tsx
@@ -321,12 +321,24 @@ export const ReaderSidebar = memo(function ReaderSidebar({
               </div>
             </div> */}
             <button
-              className="ico-btn password UI Button MultiStateButton"
+              className="ico-btn UI Button"
               data-password-active={password !== null}
               data-tip="Set/clear decryption password [K]"
               onClick={handleSetPassword}
               type="button"
-            />
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M2.5 18.5v-1h19v1zm.535-5.973l-.762-.442l.965-1.693h-1.93v-.884h1.93l-.965-1.642l.762-.443L4 9.066l.966-1.643l.761.443l-.965 1.642h1.93v.884h-1.93l.965 1.693l-.762.442L4 10.835zm8 0l-.762-.442l.966-1.693H9.308v-.884h1.93l-.965-1.642l.762-.443L12 9.066l.966-1.643l.761.443l-.965 1.642h1.93v.884h-1.93l.965 1.693l-.762.442L12 10.835zm8 0l-.762-.442l.966-1.693h-1.931v-.884h1.93l-.965-1.642l.762-.443L20 9.066l.966-1.643l.761.443l-.965 1.642h1.93v.884h-1.93l.965 1.693l-.762.442L20 10.835z"
+                ></path>
+              </svg>
+            </button>
             <a
               className="rdr-share ico-btn "
               data-tip="Copy short link [R]"
