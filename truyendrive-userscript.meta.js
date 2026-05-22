b868a9c6462cbabbb600f90b80aa38d896283baa
zennomi <zennomi@users.noreply.github.com>
2026-05-22 09:15:45 +0000

    chore(release): publish userscript 9f814b96b98ed8abab4e0e08bf4e5c21091dcac8

---
+++ truyendrive-userscript.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         TruyenDrive
+// @namespace    http://tampermonkey.net/
+// @version      2.2.0
+// @author       Zennomi
+// @description  Best Comic Reader for Google Drive
+// @icon         https://zennomi.github.io/truyendrive/icon.webp
+// @downloadURL  https://zennomi.github.io/truyendrive/truyendrive-userscript.user.js
+// @updateURL    https://zennomi.github.io/truyendrive/truyendrive-userscript.meta.js
+// @match        https://drive.google.com/drive/*
+// @match        https://drive.google.com/file/*
+// @match        https://onedrive.live.com/*
+// @require      https://cdn.jsdelivr.net/npm/systemjs@6.15.1/dist/system.min.js
+// @require      https://cdn.jsdelivr.net/npm/systemjs@6.15.1/dist/extras/named-register.min.js
+// @require      data:application/javascript,%3B(typeof%20System!%3D'undefined')%26%26(System%3Dnew%20System.constructor())%3B
+// ==/UserScript==
